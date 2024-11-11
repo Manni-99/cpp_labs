c10a2aee1c76372635add870db2634b2d13e8b5b
Kristmann Thorsteinsson <Kristmann909@gmail.com>
2024-11-11 12:24:05 +0100

    lab1 - completed

---
+++ lab1/buggy_programs/build-rel/CMakeFiles/bounds-heap.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/manni/LTH/Year3/C++/labs-edaf30/lab1/buggy_programs/bounds-heap.cc" "CMakeFiles/bounds-heap.dir/bounds-heap.cc.o" "gcc" "CMakeFiles/bounds-heap.dir/bounds-heap.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
